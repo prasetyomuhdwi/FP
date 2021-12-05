6472f9b74fe07bd0787483814457a8b72ec84a86
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2021-12-05 12:35:21 +0700

    php:create login page

--- views/login.php
+++ views/login.php
@@ -6,68 +6,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/bootstrap-5.1.3/css/bootstrap.min.css">
-    <style>
-        .login {
-            min-height: 100vh;
-        }
-
-        .bg-image {
-            background-image: url("https://source.unsplash.com/WEQbe2jBg40/600x1200");
-            background-size: cover;
-            background-position: center;
-        }
-
-        .login-heading {
-            font-weight: 300;
-        }
-
-        .btn-login {
-            font-size: 0.9rem;
-            letter-spacing: 0.05rem;
-            padding: 0.75rem 1rem;
-        }
-    </style>
+    <link rel="stylesheet" href="./assets/css/tailwindcss.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
-    <div class="container-fluid ps-md-0">
-        <div class="row g-0">
-            <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-            <div class="col-md-8 col-lg-6">
-                <div class="login d-flex align-items-center py-5">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-9 col-lg-8 mx-auto">
-                                <h3 class="login-heading mb-4">Welcome back!</h3>
-
-                                <!-- Sign In Form -->
-                                <form>
-                                    <div class="form-floating mb-3">
-                                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                                        <label for="floatingInput">Email address</label>
-                                    </div>
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                                        <label for="floatingPassword">Password</label>
-                                    </div>
-
-                                    <div class="form-check mb-3">
-                                        <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
-                                        <label class="form-check-label" for="rememberPasswordCheck">
-                                            Remember password
-                                        </label>
-                                    </div>
-
-                                    <div class="d-grid">
-                                        <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Sign in</button>
-                                        <div class="text-center">
-                                            <a class="small" href="#">Forgot password?</a>
-                                        </div>
-                                    </div>
-
-                                </form>
-                            </div>
+    <div class="min-h-screen bg-main flex flex-col justify-center sm:py-2">
+        <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+            <div class="flex justify-center items-center p-4 pb-8">
+                <img src="./assets/image/logo-alt.png" class="w-64" alt="logo">
+            </div>
+            <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+                <div class="px-5 py-7">
+                    <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
+                    <input type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                    <label class="font-semibold text-sm text-gray-600 pb-1 block">Kata Sandi</label>
+                    <input type="text" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
+                    <button type="button" class="transition duration-200 bg-green-500 hover:bg-green-600 focus:bg-green-700 focus:shadow-sm focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
+                        <span class="inline-block mr-2">Masuk</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="py-5">
+                    <div class="grid grid-cols-2 gap-1">
+                        <div class="col-start-3 col-end-7 text-center sm:text-left whitespace-nowrap">
+                            <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 inline-block align-text-top">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+                                </svg>
+                                <span class="inline-block ml-1">Lupa Kata Sandi</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="py-5">
+                <div class="grid gap-1">
+                    <div class="w-full text-center sm:text-left whitespace-nowrap">
+                        <div class="w-full flex justify-center items-center px-5 py-4 font-normal text-sm rounded-lg text-blue-50">
+                            <span>Belum punya akun?</span>
+                            <a href="/register" class="ml-2 flex hover:text-blue-800 focus:text-blue-600 cursor-pointer  transition duration-200">
+                                <b class="font-extrabold"> Registrasi Sekarang</b>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -75,10 +60,7 @@
         </div>
     </div>
 
-
-
     <script src="./assets/js/jquery-3.4.1.min.js"></script>
-    <script src="./assets/bootstrap-5.1.3/js/bootstrap.min.js"></script>
     <script src="./assets/js/app.js"></script>
 </body>
 
