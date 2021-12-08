827429dbda2782b58d5b73e8bd49ba44257b91ba
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2021-12-08 13:30:44 +0700

    php : darkmode base on cookies

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,14 @@ btn.addEventListener("click", () => {
 const body = document.querySelector("body");
 const btn_darkmode = document.querySelector("#btn_darkmode");
 const item_darkmode = document.querySelector("#item_darkmode");
-let toggle = 0;
+
+let toggle = document.cookie ? getCookies("darkmode") : false;
+
+if (toggle) {
+  btn_darkmode.classList.add("bg-green-700");
+  item_darkmode.classList.add("translate-x-7");
+  darkmode(body);
+}
 
 btn_darkmode.addEventListener("click", () => {
   btn_darkmode.classList.toggle("bg-green-700");
@@ -18,12 +25,14 @@ btn_darkmode.addEventListener("click", () => {
   darkmode(body);
 });
 function darkmode(element) {
-  if (toggle == 0) {
-    element.classList.add("dark");
-    toggle = 1;
-  } else {
+  if (toggle == false) {
     element.classList.remove("dark");
-    toggle = 0;
+    toggle = true;
+    document.cookie = "darkmode=false";
+  } else {
+    element.classList.add("dark");
+    toggle = false;
+    document.cookie = "darkmode=true";
   }
 }
 
@@ -31,10 +40,22 @@ function darkmode(element) {
 const btn_account = document.querySelector("#btn_account");
 const account_modal = document.querySelector("#account_modal");
 
-btn_account.addEventListener("click", () => {
-  account_modal.classList.remove("hidden");
-  setTimeout(() => {
-    account_modal.classList.add("hidden");
-    btn_account.blur();
-  }, 3000);
-});
+if (btn_account) {
+  btn_account.addEventListener("click", () => {
+    account_modal.classList.remove("hidden");
+    setTimeout(() => {
+      account_modal.classList.add("hidden");
+      btn_account.blur();
+    }, 3000);
+  });
+}
+
+// Cookies
+function getCookies(name) {
+  let formatedName = name + "=";
+  let value = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(formatedName))
+    .split("=")[1];
+  return value;
+}
